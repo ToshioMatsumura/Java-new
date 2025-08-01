9d7f649bdda2caa8d3cd927f063961c9f22e9cdc
toshi <toshi@ToshioPC>
2025-08-01 19:18:35 +0900

    Q9完了

--- ,Java new/src/curricculumA/Qes6.java
+++ ,Java new/src/curricculumA/Qes6.java
@@ -4,7 +4,6 @@ public class Qes6 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		//		『山田太郎 18歳 170.5cm 62.2kg 寿司』
 
 		String name = "山田太郎";
 
@@ -21,14 +20,65 @@ public class Qes6 {
 		System.out.println("年齢は" + age + "歳です");
 
 		System.out.println("身長は" + height + "cmです");
-		
+
 		System.out.println("体重は" + weight + "kgです");
 
-		System.out.println("好きな食べ物は" + food +"です");
-		
-		System.out.println("BMIは" + (weight / ((height / 100) * (height / 100)))  + "です");
-		
-		
+		System.out.println("好きな食べ物は" + food + "です");
+
+		System.out.println("BMIは" + (weight / ((height / 100) * (height / 100))) + "です");
+		//	↑↑↑↑ここまでQ6と7↑↑↑↑
+
+		name = "鈴木一郎";
+
+		age = 24;
+
+		height = 168.5;
+
+		weight = 64.2;
+
+		food = "オムライス";
+
+		System.out.println();
+		System.out.println("初めまして" + name + "です");
+
+		System.out.println("年齢は" + age + "歳です");
+
+		System.out.println("身長は" + height + "cmです");
+
+		System.out.println("体重は" + weight + "kgです");
+
+		System.out.println("好きな食べ物は" + food + "です");
+
+		System.out.println("BMIは" + (weight / ((height / 100) * (height / 100))) + "です");
+		//		↑↑↑↑ここまでQ8↑↑↑↑
+
+		name = "鈴木一郎";
+
+		age = age + 24;
+
+		height = height + 168.5;
+
+		weight = weight + 64.2;
+
+		food = "オムライス";
+
+		double bmi2 = weight / ((height / 100) * (height / 100));
+
+		System.out.println();
+		System.out.println("初めまして" + name + "です");
+
+		System.out.println("年齢は" + age + "歳です");
+
+		System.out.println("身長は" + height + "cmです");
+
+		System.out.println("体重は" + weight + "kgです");
+
+		System.out.println("好きな食べ物は" + food + "です");
+
+		String.format("%.1f", bmi2);
+
+		double bmi3 = ((double) Math.round(bmi2 * 100)) / 100;
+		System.out.println("BMIは" + bmi3 + "です");
 	}
 
 }
