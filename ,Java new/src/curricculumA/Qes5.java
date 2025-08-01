b8c7cef6b2fbf3758a0d990dc80e821673274b4e
toshi <toshi@ToshioPC>
2025-08-01 17:37:15 +0900

    Q5完了

---
+++ ,Java new/src/curricculumA/Qes5.java
@@ -0,0 +1,19 @@
+package curricculumA;
+
+public class Qes5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		/*		String num="20";
+				int num1=23;
+				System.out.println("ハローJAVA"+(num+num1));
+				このコードを修正する*/
+
+		int num = 20;
+		int num1 = 23;
+
+		System.out.println("ハローJAVA" + (num + num1));
+	}
+
+}
