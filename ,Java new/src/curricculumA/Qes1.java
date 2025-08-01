76ea7d1552961f7c714f38e9520cbee1ea1f323a
toshi <toshi@ToshioPC>
2025-08-01 16:17:30 +0900

    初期化

--- ,Java new/src/curricculumA/Qes1.java
+++ ,Java new/src/curricculumA/Qes1.java
@@ -4,7 +4,29 @@ public class Qes1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		byte  = -127;
+
+		byte b;
+		b = 10;
+
+		short s;
+		s = 11;
+
+		int num;
+		num = 12;
+		
+		long l;
+l = 13l;
+		
+		float f;
+
+		double d;
+
+		char ch;
+
+		String str;
+
+		boolean bool;
+
 	}
 
 }
