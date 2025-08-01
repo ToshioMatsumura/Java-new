cf785730d0e147c1ad6047ff4a8e59c6749f1348
toshi <toshi@ToshioPC>
2025-08-01 17:19:04 +0900

    Q4まで完了

--- ,Java new/src/curricculumA/Qes1.java
+++ ,Java new/src/curricculumA/Qes1.java
@@ -5,28 +5,82 @@ public class Qes1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		//		byte型
 		byte b;
+
+		b = 0;
+
 		b = 10;
 
+		//		short型
 		short s;
-		s = 11;
 
+		s = 0;
+
+		s = 100;
+
+		//		int型
 		int num;
-		num = 12;
-		
+
+		num = 0;
+
+		num = 1000;
+
+		//		long型
 		long l;
-l = 13l;
-		
+
+		l = 0L;
+
+		l = 10000;
+
+		//		float型
 		float f;
 
+		f = 0.0f;
+
+		f = 9.5f;
+
+		//		double型
 		double d;
 
+		d = 0.0;
+
+		d = 10.5;
+
+		//		char型
 		char ch;
 
+		ch = '\u0000';
+
+		ch = 'a';
+
+		//		String型
 		String str;
 
+		str = "あいうえお";
+
+		str = "ハロー";
+
+		//		boolean型
 		boolean bool;
 
+		bool = false;
+
+		bool = true;
+
+		System.out.println(b + s + num + l);
+
+		System.out.println(b + b);
+
+		System.out.println(ch + " " + str + " " + bool);
+
+		System.out.println(b + f + d + s + num + l);
+
+		System.out.println(b * s * num * l);
+
+		System.out.println(d / s);
+
+		System.out.println(b - s);
 	}
 
 }
