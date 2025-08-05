1228f40cb076cb5774180a596b4ea4278764b8a2
toshi <toshi@ToshioPC>
2025-08-05 15:43:44 +0900

    Q１完了

--- ,Java new/src/curriculumB/Qes1_3.java
+++ ,Java new/src/curriculumB/Qes1_3.java
@@ -1,11 +1,32 @@
 package curriculumB;
 
+import java.util.Scanner;
+
 public class Qes1_3 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		float ans = 7 / 6;
-		System.out.println(ans);
-	}
+		//		System.out.println("ユーザー名を入力してください。");
 
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("ユーザー名を入力してください: ");
+
+		String username = scanner.nextLine();
+//		入力できるようになる
+
+		if (username.length() > 10) {
+			System.out.println("名前を10文字以内にしてください");
+
+		} else if (username == null || username.length() == 0) {
+			System.out.println("名前を入力してください");
+
+		} else {
+			System.out.println("ユーザー名「" + username + "」を登録しました");
+		}
+
+		scanner.close();
+	}
 }
+
+
