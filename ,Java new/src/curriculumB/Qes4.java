bc3095539d01c5ee1fb357bc4fcc526e59c1e8f2
toshi <toshi@ToshioPC>
2025-08-06 13:16:31 +0900

    Q4完了

--- ,Java new/src/curriculumB/Qes4.java
+++ ,Java new/src/curriculumB/Qes4.java
@@ -4,7 +4,21 @@ public class Qes4 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		for (int x = 1; x <= 9; x++) {
+			for (int y = 1; y <= 9; y++) {
+				
+				String result = String.format("%02d",x * y);
+				
+//				System.out.print("0" + x + " " + "*" + " " + "0" + y + " " + "=" + " " + x * y + " || ");
 
+				if ( y == 9) {
+					System.out.print("0" + x + " " + "* 0" + y + " " + "=" + result + " ");
+				} else {
+					System.out.print("0" + x + " " + "* 0" + y + " " + " " + result + " || ");
+					
+				}
+			}
+			System.out.println();
+		}
 	}
-
 }
