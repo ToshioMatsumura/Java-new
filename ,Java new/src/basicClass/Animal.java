3fb657fe40a1d717c14647937216e640de73a7b2
toshi <toshi@ToshioPC>
2025-08-18 17:28:58 +0900

    a

--- ,Java new/src/basicClass/Animal.java
+++ ,Java new/src/basicClass/Animal.java
@@ -1,15 +1,18 @@
 package basicClass;
 
 public class Animal {
-	
+
 	public static void main(String[] args) {
-		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		  public Dog {
-			  
-		  }
-		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		
-		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+	}
+
+	// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+	public class Dog {
+		String name = "ポチ";
+		int count = 1;
 		
 	}
+
+	// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+
+	// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 }
