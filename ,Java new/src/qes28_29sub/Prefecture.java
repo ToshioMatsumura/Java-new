b681acdbd8b3392bf901990ab6c60172069f4960
toshi <toshi@ToshioPC>
2025-09-02 19:07:23 +0900

    a

--- ,Java new/src/qes28_29sub/Prefecture.java
+++ ,Java new/src/qes28_29sub/Prefecture.java
@@ -1,7 +1,26 @@
 package qes28_29sub;
 
 //フィールド（3つ）
+public class Prefecture {
 
-// コンストラクタ（引数3つを受け取る）
+	public String name;
 
-// toString() をオーバーライドして見やすく表示
+	public String capital;
+
+	public double area;
+
+	// コンストラクタ（引数3つを受け取る）
+	public Prefecture(String name, String capital, double area) {
+		this.name = name; // フィールド name に引数 name を代入
+		this.capital = capital; // フィールド capital に引数 capital を代入
+		this.area = area; // フィールド area に引数 area を代入
+	}
+
+	// toString() をオーバーライドして見やすく表示
+	@Override
+	public String toString() {
+		return "都道府県名：" + name + "\n"
+				+ "県庁所在地：" + capital + "\n"
+				+ "面積：" + area + "km2\n";
+	}
+}
