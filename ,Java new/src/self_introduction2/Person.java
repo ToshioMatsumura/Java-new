cc2b3e56b8b390db973871e916f588bf4c6c1f8c
toshi <toshi@ToshioPC>
2025-09-05 16:32:28 +0900

    修正

--- ,Java new/src/self_introduction2/Person.java
+++ ,Java new/src/self_introduction2/Person.java
@@ -1,5 +1,39 @@
+
+/*インスタンスフィールドの追加
+問題1：インスタンスフィールドに「lastName」を追加しましょう
+
+
+コンストラクタのオーバーロード
+問題2：lastNameの値を引数で受け取るコンストラクタを追加で定義してください
+
+※順番はfirstNameの次
+
+問題3：作成したコンストラクタの中に「Person.count++; this.lastName;」を追加し
+lastNameフィールドの値をセットしてください*/
 package self_introduction2;
 
-public class Person {
+class Person {
+	private String name;
+	private String lastName;
+	private int age;
+	private double height;
+	private double weight;
 
+	Person(String firstName, String lastName, int age, double height, double weight) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		Person.count++;
+	}
+
+	public double bmi() {
+		return this.weight / this.height / this.height;
+	}
+
+	public void print() {
+		System.out.println("名前は" + this.name() + "です");
+		System.out.println("年は" + this.age() + "です");
+	}
 }
